b118ae3bd9722d77bcc1bf016a1f357b3843616e
Paulo Higa <paulojhiga@gmail.com>
2025-09-05 22:16:16 -0300

    pandas
    tentando utilizar o pandas, dando ERRO!!

--- coleta_de_dados_web.py
+++ coleta_de_dados_web.py
@@ -1,14 +1,18 @@
 #https://dados.gov.br/home
 #https://br.search.yahoo.com/
 
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+#import requests
+#from bs4 import BeautifulSoup
+import pandas
 
-print("******  requests  *******")
-response = requests.get('https://dados.gov.br/home')
-print(response.text[:600])
+#print("******  requests  *******")
+#response = requests.get('https://dados.gov.br/home')
+#print(response.text[:600])
 
-print("****** BeautifulSoup *******")
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify()[:1000])
+#print("****** BeautifulSoup *******")
+#soup = BeautifulSoup(response.text, 'html.parser')
+#print(soup.prettify()[:1000])
+
+print("****** Pandas *******")
+url_dados = pandas.read_html('https://dados.gov.br/home')
+print(url_dados[0])
