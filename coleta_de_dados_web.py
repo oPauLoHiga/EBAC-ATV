e9a73fafe813a0ecab705b5fe0b9627ad162f1c0
Paulo Higa <paulojhiga@gmail.com>
2025-09-09 19:54:11 -0300

    correção
    Mudando a url.
    Pandas não estava lendo os outros links.
    não tinha tabela

--- coleta_de_dados_web.py
+++ coleta_de_dados_web.py
@@ -1,18 +1,19 @@
 #https://dados.gov.br/home
 #https://br.search.yahoo.com/
+#https://www.gov.br/esocial/pt-br/documentacao-tecnica/leiautes-esocial-versao-1-3-nt-02-2024/tabelas.html
 
-#import requests
-#from bs4 import BeautifulSoup
-import pandas
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-#print("******  requests  *******")
-#response = requests.get('https://dados.gov.br/home')
-#print(response.text[:600])
+print("******  requests  *******")
+response = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica/leiautes-esocial-versao-1-3-nt-02-2024/tabelas.html')
+print(response.text[:600])
 
-#print("****** BeautifulSoup *******")
-#soup = BeautifulSoup(response.text, 'html.parser')
-#print(soup.prettify()[:1000])
+print("****** BeautifulSoup *******")
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify()[:1000])
 
 print("****** Pandas *******")
-url_dados = pandas.read_html('https://dados.gov.br/home')
+url_dados = (pd.read_html('https://www.gov.br/esocial/pt-br/documentacao-tecnica/leiautes-esocial-versao-1-3-nt-02-2024/tabelas.html'))
 print(url_dados[0])
