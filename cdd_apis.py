335b262a27c3ffa4f7138cf6223f70d8c8c91150
Paulo Higa <paulojhiga@gmail.com>
2025-09-15 19:53:17 -0300

    Update cdd_apis.py
    não finalizada
    erros!!!

--- cdd_apis.py
+++ cdd_apis.py
@@ -1,13 +1,28 @@
 #"C:/Users/paulo/OneDrive/Documentos/teste de texto.txt"
 import requests
 
-def enviar_arquivo(url):
+def enviar_arquivo():
     caminho = 'C:/Users/paulo/OneDrive/Documentos/teste de texto.txt'
 
     #enviar o arquivo
-    requisicao = requests.post('https://gofile.io/home', files={'file': open(caminho, 'rb')})
+    requisicao = requests.post('https://api.gofile.io/v2/upload', files={'file': open(caminho, 'rb')})
     saida_requisicao = requisicao.json()
 
     print(saida_requisicao)
     url = saida_requisicao['link']
     print("Arquivo enviado. Link para acesso: ",url)
+
+def receber_arquivo(file_url):
+    #Receber o arquivo
+    requisicao = requests.get(file_url)
+
+    #Salvar o arquivo
+    if requisicao.ok:
+        with open('teste de texto.txt', 'wb') as file:
+            file.write(requisicao.content)
+        print('Arquivo baixado com sucesso!')
+    else:
+        print("Erro ao baixar o arquivo: ", requisicao.json())
+
+enviar_arquivo()
+#receber_arquivo()
