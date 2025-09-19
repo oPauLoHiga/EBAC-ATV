c4f764a2554c04982232e1bb4cb7046876249154
Paulo Higa <paulojhiga@gmail.com>
2025-09-19 15:01:44 -0300

    coreção de codigo
    erro!
    #receber arquivo
    recebe o html da pagina.
    ao ives do arquivo enviado

--- cdd_apis.py
+++ cdd_apis.py
@@ -1,15 +1,15 @@
-#"C:/Users/paulo/OneDrive/Documentos/teste de texto.txt"
+#C:/Users/paulo/Downloads/produtos_informatica.xlsx
 import requests
 
 def enviar_arquivo():
-    caminho = 'C:/Users/paulo/OneDrive/Documentos/teste de texto.txt'
+    caminho = 'C:/Users/paulo/Downloads/produtos_informatica.xlsx'
 
     #enviar o arquivo
-    requisicao = requests.post('https://api.gofile.io/v2/upload', files={'file': open(caminho, 'rb')})
+    requisicao = requests.post('https://upload.gofile.io/uploadFile', files={'file': open(caminho, 'rb')})
     saida_requisicao = requisicao.json()
 
     print(saida_requisicao)
-    url = saida_requisicao['link']
+    url = saida_requisicao['data']['downloadPage']
     print("Arquivo enviado. Link para acesso: ",url)
 
 def receber_arquivo(file_url):
@@ -18,11 +18,11 @@ def receber_arquivo(file_url):
 
     #Salvar o arquivo
     if requisicao.ok:
-        with open('teste de texto.txt', 'wb') as file:
+        with open('arquivo_baixado', 'wb') as file:
             file.write(requisicao.content)
         print('Arquivo baixado com sucesso!')
     else:
         print("Erro ao baixar o arquivo: ", requisicao.json())
 
-enviar_arquivo()
-#receber_arquivo()
+#enviar_arquivo()
+receber_arquivo('https://gofile.io/d/ofY2hX')
